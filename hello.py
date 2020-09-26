c905cc3719f3bd5647fc97299b547d0d1ad3b38c
Arno <arno.polegato@gmail.com>
2020-09-26 22:01:30 +0200

    use search function

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import tweepy
-import time 
+import time
+import csv
+from datetime import datetime, timedelta
 from settings import consumer_key, consumer_secret, access_token, access_token_secret
 
 def limit_handled(cursor):
@@ -15,10 +17,32 @@ if __name__ == '__main__':
 
     api = tweepy.API(auth)
 
-    #public_tweets = api.home_timeline()
-    #for tweet in public_tweets:
-    #    print(tweet.text) 
+    query = "blendle -filter:retweets"
 
-    for follower in limit_handled(tweepy.Cursor(api.followers).items()):
-        if follower.friends_count < 300:
-            print(follower.screen_name)
+    # 7 days ago - which is the maximum time Twitter allows you to go in the past.
+    date_since = datetime.now() - timedelta(days=7)
+    date_since = date_since.strftime("%Y-%m-%d")
+
+    with open('blendle_searches.csv', 'w', newline='') as csvfile:
+
+        csvfile = csv.writer(csvfile, delimiter=',',quotechar='"')
+        csvfile.writerow(["created_at","screen_name","text","location","favorite_count","retweet_count"])
+
+        for tweet in limit_handled(tweepy.Cursor(api.search,
+            q=query,
+            geocode="51.969685,4.051642,1000km",
+            lang="nl", 
+            rpp=100,
+            result_type="recent",
+            include_entities=True,
+            since=date_since).items(10000)):
+
+            if tweet.user.screen_name != 'BlendleNL':
+                csvfile.writerow([tweet.created_at.strftime("%Y-%m-%d"), 
+                                        tweet.user.screen_name, 
+                                        tweet.text, 
+                                        tweet.user.location, 
+                                        str(tweet.favorite_count), 
+                                        str(tweet.retweet_count)])
+                print("created_at: {}\nuser: {}\ntweet text: {}\ngeo_location: {}\nfavorite_count: {}\nretweet_count: {}".format(tweet.created_at, tweet.user.screen_name, tweet.text, tweet.user.location, tweet.favorite_count, tweet.retweet_count))
+                print("\n")
