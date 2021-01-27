4f434638c6ed95408cb78889a45a4d0c071b5a39
arno_raspberry <arno.polegato+pi@gmail.com>
2021-01-27 11:21:07 +0100

    latest changes

--- settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-# Load .env secret variables
-load_dotenv()
-
-consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
